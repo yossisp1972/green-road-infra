1e839bb871b1517b0552aa6df749edc49efb4403
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-12-18 19:09:34 +0200

    test

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.40.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.0.0"
     }
   }
 }
