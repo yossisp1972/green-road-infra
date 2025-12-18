721805e0c37e3cc6fcbb25c9133954e028fcc233
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-12-18 18:41:46 +0200

    test

--- terraform/eks-main.tf
+++ terraform/eks-main.tf
@@ -1,43 +1,62 @@
-# provider "aws" {
-#   region = var.region
-# }
+
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = var.cluster_name
+}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
+  version = "5.8.1"
 
-  name = "${var.cluster_name}-vpc"
+  name = "${local.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
-  cluster_name    = var.cluster_name
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = local.cluster_name
   cluster_version = "1.29"
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+  }
 
   eks_managed_node_groups = {
-    default = {
-      desired_size = 1
-      max_size     = 1
+    one = {
+      name = "node-group-1"
+      instance_types = ["t3.small"]
       min_size     = 1
-      instance_types = ["t3.medium"]
+      max_size     = 3
+      desired_size = 2
     }
   }
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-  sensitive = true
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
