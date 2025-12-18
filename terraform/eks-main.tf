1db76e8d32487662ee1eec0b4ab9797a87b4a638
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-12-18 19:25:09 +0200

    test

--- terraform/eks-main.tf
+++ terraform/eks-main.tf
@@ -1,9 +1,11 @@
 
+
+provider "aws" {
+  region = var.region
+}
+
 data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
+  state = "available"
 }
 
 locals {
